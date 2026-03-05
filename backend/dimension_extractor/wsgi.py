fbf00498c2b7f37e17f508599972069aa533deff
Prashanth Desai <prashanthdesai2603@gmail.com>
2026-03-05 12:22:39 +0530

    Delete backend directory

--- backend/dimension_extractor/wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
-WSGI config for dimension_extractor project.
-"""
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dimension_extractor.settings')
-application = get_wsgi_application()
